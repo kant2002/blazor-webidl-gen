c4ea0ee0b6ff2bfe68258293a01ab3e143a853e1
Andrii Kurdiumov <kant2002@gmail.com>
2018-09-27 22:37:48 +0600

    Add command for viewing parsed WebIDL

---
+++ index.ts
@@ -0,0 +1,19 @@
+import webidl2 = require("webidl2");
+import * as fs from "fs";
+import * as path from "path";
+
+const fileName = process.argv[2];
+if (!fileName) {
+    console.error("The file does not specified");
+    process.exit(1);
+}
+
+if (path.extname(fileName) != ".widl") {
+    console.error(`The file ${fileName} should have extension .widl`);
+    process.exit(1);
+}
+
+
+const content = fs.readFileSync(fileName);
+const parsingStructure = webidl2.parse(content.toString());
+console.log(JSON.stringify(parsingStructure, null, 2));
